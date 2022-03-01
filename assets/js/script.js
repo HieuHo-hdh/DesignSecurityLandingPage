ade0a4461ff95b587eaf12c18646d23ffa7e5a03
HieuHo-hdh <62245214+HieuHo-hdh@users.noreply.github.com>
2022-03-01 19:53:26 +0700

    Fix Styles by comments, Add javascript, Add hover button
    Fix Styles followed by Figma Comments
    Add hover to button (description, benefit, navigation)
    Add decoration frames (1 left) - benefit
    Add js to button (description, benefit) to show more
    Add js to route to another page

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// Show more description
+function showMoreDescription() {
+    var moreDescription = document.getElementById("home-description");
+    var moreDescriptionButton = document.getElementById("home-description-button");
+    moreDescription.style.display = "block";
+    moreDescription.style.maxHeight = "unset";
+    moreDescriptionButton.style.display = "none";
+}
+
+// Show 2 last cards - motivation cards
+function showHiddenMotivationCard() {
+    var reverseEngineering = document.getElementById("reverse-engineering");
+    var manyMore = document.getElementById("many-more");
+    var buttonMore = document.getElementById("button-more");
+    reverseEngineering.style.display = "block";
+    manyMore.style.display = "block";
+    buttonMore.style.display = "none";
+}
+
+// OnClick => Route to another page
+
+document.getElementById("consult-now").onclick = function () {
+    location.href = "https://www.google.com";
+};
+
+document.getElementById("contact-us").onclick = function () {
+    location.href = "https://www.google.com";
+};
